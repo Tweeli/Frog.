e79acaaea629e11293ef0a22262d7d1468b077b3
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-06 21:00:15 +0200

    Create bapTag.js

---
+++ commands/bapTag.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    message.send("BAP#0515")
+
+}
+
+module.exports.help = {
+    name: "baptag"
+}
