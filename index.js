324adc795adceeae2203ba0a0161a4bfe11eab85
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-07 20:35:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.login(botConfig.token);
 bot.on("ready", async () => {
     
 console.log(`${bot.user.username} Is online!`)
-bot.user.setActivity("Created by Tweeli.#0001.", {type: "CUSTOM_STATUS"});
+bot.user.setActivity("naar Yana haar gezaag..", {type: "LISTENING"});
 
 });
 
