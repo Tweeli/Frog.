e8458403b555bbfbd0d52cc7a03e890974f68570
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-07 17:29:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.login(botConfig.token);
 bot.on("ready", async () => {
     
 console.log(`${bot.user.username} Is online!`)
-bot.user.setActivity("BapKuip is stout!.", {type: "PLAYING"});
+bot.user.setActivity("BapKuip is stout!", {type: "PLAYING"});
 
 });
 
