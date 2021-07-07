b368f722708e2eb413813853bd4aced9c6aa824d
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-07 19:29:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.login(botConfig.token);
 bot.on("ready", async () => {
     
 console.log(`${bot.user.username} Is online!`)
-bot.user.setS("Created by Tweeli.#0001.", {type: "PLAYING"});
+bot.user.setActivity("Created by Tweeli.#0001.", {type: "PLAYING"});
 
 });
 
