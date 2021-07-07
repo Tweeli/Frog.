d48ac469a9462de5a524ccdd660a2192e312bcca
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-07 20:31:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.login(botConfig.token);
 bot.on("ready", async () => {
     
 console.log(`${bot.user.username} Is online!`)
-bot.user.setActivity("Yana is stom.", {type: "WATCHING"});
+bot.user.setActivity("Yana.", {type: "WATCHING"});
 
 });
 
