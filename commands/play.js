4de1c6d779ed561d1748cd1756093708bd6bf7ab
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-07 23:34:19 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,11 +1,35 @@
 const discord = require("discord.js");
+const ytdl = require("ytdl-core");
 
 module.exports.run = async (bot, message, args) => {
 
-    
+    if (!message.member.voice.channel) return message.reply("> Join eerst een spraakkanaal.");
+
+    if (message.guild.me.voice.channel) return message.channel.send("> Sorry, de bot is al verbonden.");
+
+    if (!args[0]) return message.reply("> Gelieven een url mee te geven.");
+
+    var validate = await ytdl.validateURL(args[0]);
+    if(!validate) return message.channel.send("> Sorry, gelieve een juiste url op te geven.");
+
+    var info = await ytdl.getInfo(args[0]);
+
+    var options = { seek: 3, volume: 1 };
+
+    var channeJoin = message.member.voice.channel.join()
+        .then(voiceChannel => {
+
+            var stream = ytdl(args[0], { filter: 'audioonly' });
+            var dispatcher = voiceChannel.play(stream, options);
+
+        }).catch(console.error);
+
+    message.channel.send(`Nu aan het spelen ${info.title}`);
 
 }
 
 module.exports.help = {
-    name: "play"
+    name: "play",
+    description: "Speel muziek af met deze command!",
+    catagory: "Muziek."
 }
