80eb10703037d47244565226eabf82f247a80254
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-08 20:56:05 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -14,6 +14,7 @@ module.exports.run = async (bot, message, args, options) => {
     if(!validate) return message.channel.send("Sorry, gelieve een juiste url op te geven.");
 
     var info = await ytdl.getInfo(args[0]);
+        if(err) console.log(err);
 
     var data = options.active.get(message.guild.id) || {};
 
