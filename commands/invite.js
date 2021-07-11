6af07383f8bd8208aedb005f2db1cea090639488
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-11 20:40:37 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ module.exports.run = async(bot, message, args) => {
 
     var inviteEmbed = new discord.MessageEmbed()
      .setTitle("Invites!")
-     .setDescription('[Frog. Discord bot server.](https://discord.gg/c9nfzh6zTu) \n [TwoepKoep chats.](https://discord.gg/VrWyrpkM6B)')
+     .setDescription('[Frog. Discord bot server.](https://discord.gg/c9nfzh6zTu) \n - \n [TwoepKoep chats.](https://discord.gg/VrWyrpkM6B) \n -')
      .setColor("#6aa75e")
      .setFooter('Created by Tweeli.#0001');
     message.channel.send(inviteEmbed)   
