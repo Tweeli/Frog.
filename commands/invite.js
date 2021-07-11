9b90efb07a3d3c866ed2e2ae5a93e31ef3dbef3b
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-11 20:48:09 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ module.exports.run = async(bot, message, args) => {
 
     var inviteEmbed = new discord.MessageEmbed()
      .setTitle("Invites!")
-     .setDescription('[Frog. Discord bot server.](https://discord.gg/c9nfzh6zTu) \n - \n [Belsterdam RolePlay info.](https://discord.gg/c9JfpqEgsD)')
+     .setDescription('[Frog. Discord bot server.](https://discord.gg/c9nfzh6zTu) \n - \n [TwoepKoep chats.](https://discord.gg/VrWyrpkM6B) \n - \n [Belsterdam RolePlay info.](https://discord.gg/c9JfpqEgsD)')
      .setColor("#6aa75e")
      .setFooter('Created by Tweeli.#0001');
     message.channel.send(inviteEmbed)   
