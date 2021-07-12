9ac63be53af9c2e8465ca7e9b01476fd329cc04f
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-12 20:30:08 +0200

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -1,25 +1,21 @@
-const discord = require("discord.js");
-
-module.exports.run = async (bot, message, args, options) => {
+module.exports.run = async (client, message, args, options) => {
 
     var guildIDData = options.active.get(message.guild.id);
 
-    if (!guildIDData) return message.reply("Er is geen muziek aan het spelen op dit moment.");
+    if (!guildIDData) return message.channel.send("Er is geen muziek aan het spelen op dit moment.");
 
-    if (message.member.voice.channel != message.guild.me.voice.channel) return message.reply("Sorry, je zit niet in het zelfde spraakkanaal.");
+    if (message.member.voice.channel != message.guild.me.voice.channel) return message.channel.send("Sorry je zit niet in het zelfde kanaal");
 
-    if (isNaN(args[0]) || args[0] > 150 || args[0] < 0) return message.reply("Gelieve een nummer op te geven tussen O - 150");
+    if (isNaN(args[0]) || args[0] > 150 || args[0] < 0) return message.reply("Gelieve een nummer op te geven tussen 0 - 150");
 
     guildIDData.dispatcher.setVolume(args[0] / 100);
-    
-    return message.reply(`Volume aangepast naar ${args[0]}`);
 
-
+    return message.channel.send(`Volume aangepast naar ${args[0]}`);
 
 }
 
 module.exports.help = {
     name: "volume",
-    description: "Met deze command kan je de muziek luider of zachter zetten!",
-    category: "Muziek."
+    description: "Geeft al de verschillende commands",
+    category: "Informatie"
 }
