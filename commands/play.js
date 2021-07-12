6c0e1ee2790395dd83adcf5429d246a6108f1718
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-12 20:25:13 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,25 +1,24 @@
 const discord = require("discord.js");
-const { attachCookies } = require("superagent");
 const ytdl = require("ytdl-core");
 
-module.exports.run = async (bot, message, args, options) => {
+module.exports.run = async (client, message, args, options) => {
 
-    if (!message.member.voice.channel) return message.reply("Join eerst een spraakkanaal.");
+    if (!message.member.voice.channel) return message.reply("Connecteer met een spraak kanaal");
 
-    //if (message.guild.me.voice.channel) return message.channel.send("Sorry, de bot is al verbonden.");
+    // if (message.guild.me.voice.channel) return message.channel.send("Sorry, de bot is al verbonden.");
 
-    if (!args[0]) return message.reply("Gelieven een url mee te geven.");
+    if (!args[0]) return message.reply("Gelieven een url mee te geven");
 
     var validate = await ytdl.validateURL(args[0]);
-    if(!validate) return message.reply("> Sorry, gelieven een juiste url op te geven.");
+    if (!validate) return message.channel.send("Sorry, Gelieve een **juiste** url op te geven.");
 
     var info = await ytdl.getInfo(args[0]);
-    
+
     var data = options.active.get(message.guild.id) || {};
 
-    if(!data.connection) data.connection = await message.member.voice.channel.join();
+    if (!data.connection) data.connection = await message.member.voice.channel.join();
 
-    if(!data.queue) data.queue = [];
+    if (!data.queue) data.queue = [];
 
     data.guildID = message.guild.id;
 
@@ -32,58 +31,71 @@ module.exports.run = async (bot, message, args, options) => {
 
     });
 
-    if(!data.dispatcher){
-        Play(bot, options, data);
-    }else{
-        message.reply(`Toegevoegd aan queue: ${info.title} | Aangevraagd door: ${message.author.tag}`);
+    if (!data.dispatcher) {
+        Play(client, options, data);
+    } else {
+        message.channel.send(`Toegevoegd aan de queue: ${info.title} | Aangevraagd door: ${message.author.tag}`);
     }
 
     options.active.set(message.guild.id, data);
 
+    // var options = { seek: 3, volume: 1 };
+
+    // var channelJoin = message.member.voice.channel.join()
+    //     .then(voiceChannel => {
+
+    //         var stream = ytdl(args[0], { filter: 'audioonly' });
+    //         var dispatcher = voiceChannel.play(stream, options);
+
+    //     }).catch(console.error);
+
+    // message.channel.send(`Nu aan het spelen ${info.title}`);
+
 }
 
-async function Play(bot, ops, data){
+async function Play(client, ops, data) {
 
-    bot.channels.cache.get(data.queue[0].announceChannel).send(`Nu aan het spelen: ${data.queue[0].songTitle} - aangevraagd door: ${data.queue[0].requester}`);
+    client.channels.cache.get(data.queue[0].announceChannel).send(`Nu aan het spelen: ${data.queue[0].songTitle} - aangevraagd door: ${data.queue[0].requester}`);
 
     var options = { seek: 2, volume: 1, bitrate: 128000 };
 
-    data.dispatcher = await data.connection.play(ytdl(data.queue[0].url, {filter: 'audioonly'}), options);
+    data.dispatcher = await data.connection.play(ytdl(data.queue[0].url, { filter: 'audioonly' }), options);
 
     data.dispatcher.guildID = data.guildID;
 
     data.dispatcher.once('finish', function () {
-        Finish(bot, ops, this);
+        Finish(client, ops, this);
     })
 
 }
 
-function Finish(bot, ops, dispatcher){
+function Finish(client, ops, dispatcher) {
 
     var fetechedData = ops.active.get(dispatcher.guildID);
 
     fetechedData.queue.shift();
 
-    if(fetechedData.queue.length > 0){
+    if (fetechedData.queue.length > 0) {
 
         ops.active.set(dispatcher.guildID, fetechedData);
 
-        Play(bot, ops, fetechedData);
+        Play(client, ops, fetechedData);
 
-    }else{
+    } else {
 
         ops.active.delete(dispatcher.guildID);
 
-        var voiceChannel = bot.guilds.cache.get(dispatcher.guildID).me.voice.channel;
+        var voiceChannel = client.guilds.cache.get(dispatcher.guildID).me.voice.channel;
 
-        if(voiceChannel) voiceChannel.leave();
+        if (voiceChannel) voiceChannel.leave();
 
     }
 
 }
 
+
 module.exports.help = {
     name: "play",
-    description: "Speel muziek af met deze command!",
-    catagory: "Muziek."
+    description: "Geeft al de verschillende commands",
+    category: "Informatie"
 }
